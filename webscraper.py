d0101e589d7155cfb3ebf0099d10826aa51f335d
MnmP12 <126064159+MnmP12@users.noreply.github.com>
2023-03-29 16:06:31 +1100

    webscraper update
    removed link in scraper so inputting your own link is more clear.

--- webscraper.py
+++ webscraper.py
@@ -1,16 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-url =  "https://www.abc.net.au/news/2023-02-27/khal-asfour-expenses-out-of-step-with-community/102030196"  #Add any abc news article link here.
+url = input("Enter ABC NEWS URL here : ")  #Add any abc news article link here.
 html_source = requests.get(url)         #Retrieving abc news links source code.
 
 soup = BeautifulSoup(html_source.content, 'html5lib')    # Gathering elements within the link.
-for title in soup.find_all('title'):            
+for title in soup.find_all('title'):           
     print(title.get_text())
 #Extracts Title of article.
 
 text = ''
 for text in soup.find_all(class_='_39n3n'):
-    print(text.get_text())
+    print(text.get_text()) 
 #Extracts Content of article.                   
 
