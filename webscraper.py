67004be31627a814646f8e9a049b68bedb4fb62a
MnmP12 <126064159+MnmP12@users.noreply.github.com>
2023-04-03 04:01:49 +1000

    11
    did something

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,7 @@
 import requests
+from pprint import pprint
 from bs4 import BeautifulSoup
+
 
 url = input("Enter ABC NEWS URL: ")  # Prints the instructions to CMD
 html_source = requests.get(url)
@@ -7,7 +9,7 @@ soup = BeautifulSoup(html_source.content, 'html5lib')
 text = ''
 
 for title in soup.find_all('title'):  # This line of code retrieves the article
-    print(title.get_text())
+    pprint(title.get_text())
 
 for text in soup.find_all(class_='_39n3n'):  # This returns the content
-    print(text.get_text())
+    pprint(text.get_text())
