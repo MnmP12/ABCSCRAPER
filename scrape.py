ea8ac536eb9141259bee404638b9f38f930255d1
MnmP12 <126064159+MnmP12@users.noreply.github.com>
2023-05-10 15:36:20 +1000

    Run this for the scraper

--- scrape.py
+++ scrape.py
@@ -1,17 +1,46 @@
+import PySimpleGUI as sg
 import requests
 from bs4 import BeautifulSoup
 
 
-# Accessing source code/gathering elements of html page
-url = input("Enter ABC NEWS URL: ")
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-text = ''
+# Creates first window
+def make_first_win():
+    sg.theme("DarkBrown5")
+    layout = [[sg.Text('Enter ABC News article URL:'), sg.InputText()],
+              [sg.Button('View')]]
+    return sg.Window("ABCSCRAPER", layout, finalize=True)
 
-# Retrieves title of article 
-for title in soup.find('title'):  
-    print(title.get_text())
 
-# Returns body of article
-for text in soup.find_all(class_="paragraph_paragraph__3Hrfa"):  
-    print(text.get_text())
+# Code to scrape ABC url in second window
+def make_second_win(url):
+    sg.theme("DarkBrown5")
+    # Gathers source code
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    # Finds title/body text of page
+    title_find = soup.find("title").get_text()
+    infos = soup.find_all(class_="paragraph_paragraph__3Hrfa")
+    body_find = "\n\n".join([info.get_text() for info in infos])
+    # Initalizes measurements of window
+    layout = [[sg.Text(title_find, size=(70, 0))],
+              [sg.Multiline(body_find, size=(80, 30))]]
+    return sg.Window("Article", layout, finalize=True)
+
+
+# Ensures only first window opens when file is run
+window1, window2 = make_first_win(), None
+
+# While loop to close second window, exit program if first window is closed
+while True:
+    window, event, values = sg.read_all_windows()
+    if event == sg.WIN_CLOSED:
+        window.close()
+        if window == window2:
+            window2 = None
+        elif window == window1:
+            break
+# If view is clicked, article is scraped in second window
+    if event == 'View' and not window2:
+        url = values[0]
+        if url:
+            window2 = make_second_win(url)
