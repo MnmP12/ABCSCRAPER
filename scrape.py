e177e66cadb21af0bdb04cb0bf35c67ad900fa6f
MnmP12 <126064159+MnmP12@users.noreply.github.com>
2024-04-02 22:49:41 +1100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -5,13 +5,13 @@ from bs4 import BeautifulSoup
 # Function to create the main window
 def make_first_win():
     sg.theme("DarkBrown5")
-    layout = [[sg.Text('Top 5 Stories of the day:')]]
+    layout = [[sg.Text('Top 5 ABC articles today:', size=(20, 1), justification='center')]]
     
     # Add buttons labeled with numbers 1 to 5
-    for i in range(1, 6):
-        layout.append([sg.Button(str(i), key=f'button_{i}', size=(4, 1))])
+    button_layout = [[sg.Button(str(i), key=f'button_{i}', size=(4, 1), pad=(2, 2), button_color=('white', sg.theme_background_color()))] for i in range(1, 6)]
+    layout.append([sg.Column(button_layout, element_justification='center', vertical_alignment='center')])
     
-    return sg.Window("ABCSCRAPER", layout, finalize=True, grab_anywhere=True)  # Make the window movable
+    return sg.Window("ABCSCRAPER", layout, finalize=True, grab_anywhere=True, size=(200, 200))  # Make the window movable
 
 # Main code
 window1 = make_first_win()
@@ -23,6 +23,8 @@ while True:
     elif event.startswith('button_'):
         selected_index = int(event.split('_')[1])
         print(f"You pressed button {selected_index}")
+
+
 
 
 
